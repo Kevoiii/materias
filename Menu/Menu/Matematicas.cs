93631139a1e1ab9c0d019385999ba7edea2ba8bd
Cerealfn <carm080407hgtmmga1@cbtis217.edu.mx>
2024-10-24 16:31:56 +0200

    Primer paso

---
+++ Menu/Menu/Matematicas.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC15
+ * Date: 24/10/2024
+ * Time: 04:27 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Menu
+{
+	/// <summary>
+	/// Description of Matematicas.
+	/// </summary>
+	public partial class Matematicas : Form
+	{
+		public Matematicas()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
