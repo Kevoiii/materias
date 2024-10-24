95fde951529b8c925e056a37d9be7d85c1202967
AJ15FG <fega080415mgtrzma5@cbtis217.edu.mx>
2024-10-24 08:33:53 -0600

    Materia: física

---
+++ Menu/Menu/Física.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC12
+ * Date: 24/10/2024
+ * Time: 08:30 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms; 
+
+namespace Menu
+{
+	/// <summary>
+	/// Description of Física.
+	/// </summary>
+	public partial class Física : Form
+	{
+		public Física()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
