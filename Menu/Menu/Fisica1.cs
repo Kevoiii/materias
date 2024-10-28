03f3bf6a47dc9cf9e3beaa79bad4f431485b48c2
AJ15FG <fega080415mgtrzma5@cbtis217.edu.mx>
2024-10-28 08:30:43 -0600

    Materia f

---
+++ Menu/Menu/Fisica1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 28/10/2024
+ * Time: 08:29 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Menu
+{
+	/// <summary>
+	/// Description of Fisica1.
+	/// </summary>
+	public partial class Fisica1 : Form
+	{
+		public Fisica1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
