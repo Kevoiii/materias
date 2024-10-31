fe885bde768a1f76cd54fa0d38c87595fca8ead6
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-10-30 21:14:52 -0600

    Boton de audio listo

--- Menu/Menu/ecosistemas.cs
+++ Menu/Menu/ecosistemas.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
 
 namespace Menu
 {
@@ -16,13 +17,16 @@ namespace Menu
 	/// Description of ecosistemas.
 	/// </summary>
 	public partial class ecosistemas : Form
+		
 	{
+		SpeechSynthesizer audio;
 		public ecosistemas()
 		{
 			//
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
+			audio = new SpeechSynthesizer();
 			
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
@@ -32,7 +36,12 @@ namespace Menu
 	
 		void BtnvolverClick(object sender, EventArgs e)
 		{
-			this.Close(); 
+			this.Close();  
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			string audio1 ="En resumen, la ecología busca comprender cómo funciona la naturaleza y cómo los seres humanos pueden vivir en armonía con el planeta, nuestro hogar, y al mismo tiempo es como cuidarlo. Siendo el estudio de cómo los seres vivos interactúan entre sí y con su entorno, abarcando: Relaciones entre organismos, Interacciones con el medio ambiente, Dinámicas de poblaciones y ecosistemas, Impactos humanos en la naturaleza, Conservación y gestión de recursos";
+			audio.Speak(audio1);
 		}
 	}
 }
